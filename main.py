10a6a8f3f760375dd6e5fa94375c846c0b149ad9
lfabbri98 <luca.fabbri98@outlook.it>
2021-11-17 12:47:44 +0100

    Added filling fraction data

--- main.py
+++ main.py
@@ -23,18 +23,24 @@ for x in range(4):
 ###############################################################################
 matrix = system_creation(init_data)
 t = 0 #time counter
-domain_coordinates = generate_pos_table(init_data)
+domain_coordinates = generate_pos_table(init_data) #table with coordinated of all points
 number_domains = 0 #domains counter
+table_filled_fraction = np.zeros((init_data[0]**init_data[1],2)) #table with filled fraction in function of time
+counter_table = 0 #counter for filling previous table
 
 while number_domains < init_data[0]**init_data[1]:
     
+    #2D case
     if init_data[1] == 2:
         matrix, domain_coordinates, number_domains = nucleation_2D(matrix, init_data, domain_coordinates, number_domains)
         
-    if init_data[1] == 3:
+    #3D case
+    elif init_data[1] == 3:
         matrix, domain_coordinates, number_domains = nucleation_3D(matrix, init_data, domain_coordinates, number_domains)
     
-    t = t+1
+    t = t+1 #increase time after each step
+    table_filled_fraction[counter_table] = [t,number_domains/init_data[0]**init_data[1]] #update filling fraction
+    counter_table = counter_table+1
     print("Progession: ",round(number_domains/init_data[0]**init_data[1]*100,3),"%" )
     
     
