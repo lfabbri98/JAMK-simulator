aaff76b2f4ba428a6ff6484be79a76cafe140e39
lfabbri98 <luca.fabbri98@outlook.it>
2021-11-21 18:39:42 +0100

    Added fit

--- data_analysis.py
+++ data_analysis.py
@@ -3,10 +3,14 @@ import numpy as np
 from scipy.optimize import curve_fit
 import time
 
+def poly1(x,A,B):
+    return A+B*x
+
 def plot_JMAK(table,counter_table,params,name):
     #import of table and date to save figure
     table = table[table[:,1]!=0]
-    assert len(table) == counter_table
+    table = table[table[:,1]!=1]
+    assert len(table) == counter_table -1
     #plotting
     plt.figure()
     plt.plot(table[:,0],table[:,1])
@@ -15,6 +19,15 @@ def plot_JMAK(table,counter_table,params,name):
     plt.grid();
     plt.title("Simulated JMAK kinetic for N="+str(params[0])+", n="+str(params[1])+", J="+str(params[2])+", R="+str(params[3]))
     plt.savefig('./Outputs/'+name+'/'+name+'-JMAK.png')
+    plt.close()
+    
+    #fitting
+    median_table = int(counter_table/2)
+    X = np.log(table[:,0])
+    Y = np.log(-np.log(1-table[:,1]))
+    popt, pcov = curve_fit(poly1,X,Y)
+    print("Fitted with linear model A+B*x \n A= "+str(popt[0])+"\n B= "+str(popt[1]))
+
 
 def plot_matrix(table_positions, params, fraction,name):
     if params[1] == 3:
@@ -28,3 +41,5 @@ def plot_matrix(table_positions, params, fraction,name):
     frac = round(fraction*100) 
     plt.title("Matrix at "+str(frac)+" %")
     plt.savefig('./Outputs/'+name+'/'+name+'-MATRIX-'+str(frac)+'.png')
+    plt.close()
+    
