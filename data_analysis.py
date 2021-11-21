420740847d513b498af1290ede7c79289f91ae86
lfabbri98 <luca.fabbri98@outlook.it>
2021-11-21 16:34:36 +0100

    Added function plot_matrix to plot a screenshot of the matrix

--- data_analysis.py
+++ data_analysis.py
@@ -15,5 +15,18 @@ def plot_JMAK(table,counter_table,params):
     plt.ylabel("$\eta$")
     plt.grid();
     plt.title("Simulated JMAK kinetic for N="+str(params[0])+", n="+str(params[1])+", J="+str(params[2])+", R="+str(params[3]))
-    plt.savefig('./Outputs/'+date+'.png')
+    plt.savefig('./Outputs/'+date+'-JMAK.png')
+
+def plot_matrix(table_positions, params, fraction):
+    date = time.strftime("%Y-%m-%d-%H-%M-%S")
+    if params[1] == 3:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.scatter(table_positions[:,0],table_positions[:,1], table_positions[:,2])
     
+    if params[1] == 2:       
+        plt.scatter(table_positions[:,0],table_positions[:,1])
+        
+    frac = round(fraction*100) 
+    plt.title("Matrix at "+str(frac)+" %")
+    plt.savefig('./Outputs/'+date+'-MATRIX-'+str(frac)+'.png')
