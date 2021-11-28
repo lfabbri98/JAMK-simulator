72b24349a11f30f07f2d5169622c65e15922ffeb
lfabbri98 <luca.fabbri98@outlook.it>
2021-11-28 21:20:01 +0100

    Implemented new function nucleation in JMAk file which works with both 2D and 3D

--- JMAK.py
+++ JMAK.py
@@ -1,53 +1,52 @@
 import numpy as np
-import random 
-from controls import *
+from numpy import random
 
-"""
-Note:
-    params[0] = N
-    params[1] = n
-    params[2] = J
-    params[3] = R
-"""
+def nucleation(matrix, N, dim, J, seed, positions_nuclei, num_nuclei):
+    """
 
+    Parameters
+    ----------
+    matrix : array of int
+        matrix of the system
+    N : int
+        side length of matrix
+    dim : int
+        dimensionality of the process
+    J : int
+        nucleation rate, how many new nuclei are generated in the matrix at each
+        time step
+    seed : int
+        seed for random number generation
+    positions_nuclei : array of int
+        stores the positions of all nucleated domains
+    num_nuclei : int
+        number of total nuclei in the matrix
 
-def nucleation_2D(matrix, params, positions_nuclei, num_nuclei):
-    random.seed(1)
-    internal_matrix = matrix
+    Returns
+    -------
+    matrix : array of int
+        matrix of the system after nucleation process
+    positions_nuclei : array of int
+       stores the positions of all nucleated domains with in addition the new
+       nucleated ones
+    num_nuclei : int
+        number of total nuclei in the matrix after nucleation
+
+    """
+    random.seed(seed)
     num_cycled=0
-    while num_cycled < params[2]:
-        new_position_1 = random.randint(0,params[0]-1)
-        new_position_2 = random.randint(0,params[0]-1)
-        if internal_matrix[new_position_1, new_position_2] == 1:
-            if is_all_full(internal_matrix, params):
-                break
+    while num_cycled < J:
+        new_position = random.randint(0,N,dim)
+        if matrix[tuple(new_position)] == 1: 
             continue
-        internal_matrix[new_position_1, new_position_2] = 1
-        positions_nuclei[num_nuclei] = [new_position_1, new_position_2]
+        matrix[tuple(new_position)] = 1
+        positions_nuclei[num_nuclei] = new_position
         num_cycled = num_cycled+1
         num_nuclei = num_nuclei+1
 
-    return internal_matrix, positions_nuclei, num_nuclei
+    return matrix, positions_nuclei, num_nuclei
 
-def nucleation_3D(matrix, params, positions_nuclei, num_nuclei):
-    random.seed(1)
-    internal_matrix = matrix
-    num_cycled=0
-    while num_cycled < params[2]:
-        new_position_1 = random.randint(0,params[0]-1)
-        new_position_2 = random.randint(0,params[0]-1)
-        new_position_3 = random.randint(0,params[0]-1)
-        if internal_matrix[new_position_1, new_position_2,new_position_3] == 1:
-            if is_all_full(internal_matrix, params):
-                break
-            continue
-        internal_matrix[new_position_1, new_position_2, new_position_3] = 1
-        positions_nuclei[num_nuclei] = [new_position_1, new_position_2, new_position_3]
-        num_cycled = num_cycled+1
-        num_nuclei = num_nuclei+1
-
-    return internal_matrix, positions_nuclei, num_nuclei
-
+"""
 def growth_2D(matrix, params, positions_nuclei, num_nuclei):
     if is_all_full(matrix,params):
         return [matrix, positions_nuclei, num_nuclei]
@@ -83,26 +82,26 @@ def growth_3D(matrix, params, positions_nuclei, num_nuclei):
                     x = positions_nuclei[q,0] + w
                     y = positions_nuclei[q,1] + s
                     z = positions_nuclei[q,2] + t
-                #controls for segmentantion fault
-                if x>params[0]:
-                    x = abs(params[0]-x)
-                if x<0:
-                    x = params[0] - abs(1-x)
-                if y>params[0]:
-                    y = abs(params[0]-y)
-                if y<0:
-                    y = params[0] - abs(1-y)  
-                if z>params[0]:
-                    z = abs(params[0]-z)
-                if z<0:
-                    z = params[0] - abs(1-z)  
+                    #controls for segmentantion fault
+                    if x>params[0]:
+                        x = abs(params[0]-x)
+                    if x<0:
+                        x = params[0] - abs(1-x)
+                    if y>params[0]:
+                        y = abs(params[0]-y)
+                    if y<0:
+                        y = params[0] - abs(1-y)  
+                    if z>params[0]:
+                        z = abs(params[0]-z)
+                    if z<0:
+                        z = params[0] - abs(1-z)  
                 
-                if matrix[int(x)-1,int(y)-1, int(z)-1] == 0:
-                    matrix[int(x)-1,int(y)-1,int(z)-1] = 1
-                    positions_nuclei[num_nuclei] = [x,y,z]
-                    num_nuclei = num_nuclei+1
+                    if matrix[int(x)-1,int(y)-1, int(z)-1] == 0:
+                        matrix[int(x)-1,int(y)-1,int(z)-1] = 1
+                        positions_nuclei[num_nuclei] = [x,y,z]
+                        num_nuclei = num_nuclei+1
     return matrix, positions_nuclei, num_nuclei
-
+"""
 
  
     
