Corrected a bug that used to proved a wrong shape od domains

JMAK.py
@@ -89,13 +89,13 @@ def growth(matrix,N,dim,R,positions_nuclei, num_nuclei):
     for x in range(0,num_nuclei):
         #select current position of single nucleus to grow around it
         current_position = positions_nuclei[x]
-        change_position = current_position
         #repeat the step for all dimensions
         for i in range(0,dim):
+            change_position = current_position
             #repeat for each value between -R and R
             for j in range(-R,R+1):
                 change_position = np.array(change_position)
-                change_position[i] = (current_position[i] + j)%N
+                change_position[i] = (current_position[i] + j)%N 
                 change_position = tuple(change_position)
                 #if cell is empty then fill it and add change_position to position_nuclei table
                 if matrix[change_position] == 0: